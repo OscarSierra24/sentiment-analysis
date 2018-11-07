68de39e096fcc0b71cd719b966b9f0b5d4dcfeab
Daniela Güemez <danielaguemez@gmail.com>
2018-11-07 11:51:16 -0600

    Add files via upload

---
+++ get_tt.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  3 17:24:32 2018
+
+@author: daniela
+"""
+import tweepy
+import json
+
+CONSUMER_KEY = 'So62vN1g4kNaNTPhv79yWLoU1'
+CONSUMER_SECRET = 'blzEA9UaMpaczTL5HD3EqReKOSBGfdZXUIC00nP8POjMwtezj3'
+OAUTH_TOKEN = '720704846135857152-hhGcmCCst5jag6GznKJw6zEwanItklv'
+OAUTH_TOKEN_SECRET = 'UJnFf8EzQPJ4BNyB0jz5jvAomsufqMLKhbxF0T0gAwQAQ'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+def get_trends():
+    trends1 = api.trends_place(1) # 1 is for WW trends
+    data = trends1[0] 
+    trends = data['trends']
+#    grab the name from each trend
+    names = [trend['name'] for trend in trends]
+    return names
+
+print(get_trends())
+
+"""from langdetect import detect_langs
+
+def english(tt):
+    new=[]
+    for item in tt:
+        #print(item)
+        tmp=detect_langs(item)
+        if (tmp == "en"):
+            print(item)
+            new.append()
+    return new
+
+print(english(names))"""
